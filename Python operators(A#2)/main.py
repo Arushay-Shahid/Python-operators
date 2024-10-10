e4161f46bfd24a7e2634da6648aaf6f2d810c9bf
Arushay-Shahid <ummearushay567@gmail.com>
2024-10-10 21:17:59 +0500

    Add files via upload

---
+++ Python operators(A#2)/main.py
@@ -0,0 +1,16 @@
+a=5
+b=5
+c=5
+print("a!=b")
+print("b!=c")
+a= "hello"
+b="welcome"
+if a!=b:
+    print("I am fine")
+    a=6
+    b=7
+    a=int(input("enter a number:"))
+    if a%2 !=0:
+        print(a,"is an odd number")
+    else:
+        print(a,"is a even number")
